f74d2a8878251cc9e3ee36eb05b0f6682b8c6af4
Pawan Sasanka <pawan@duosoftware.com>
2017-11-24 13:21:45 +0530

    unused required module removed

--- EventPublisher.js
+++ EventPublisher.js
@@ -1,7 +1,6 @@
 /**
  * Created by Pawan on 11/24/2017.
  */
-var format = require('stringformat');
 var config = require('config');
 var amqp = require('amqp');
 var logger = require('dvp-common/LogHandler/CommonLogHandler.js').logger;
